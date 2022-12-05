f003eb8347c3ce5d37c00232808852054aba9614
par55 <par555@mail.ru>
2022-12-05 23:19:57 +0100

    HomeWork9

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+    public class Main {
+    public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            int number = scanner.nextInt();
+            answer(number);
+        }
+        public static int firstNum(int number) {
+            int a = number / 100;
+            return a;
+        }
+        public static int secondNum(int number) {
+            int b = number / 10 % 10;
+            return b;
+        }
+        public static int thirdNum(int number) {
+            int c = number % 10;
+            return c;
+        }
+        public static void answer(int number){
+            System.out.println("Первое число - " + firstNum(number) + ".");
+            System.out.println("Второе число - " + secondNum(number) + ".");
+            System.out.println("Третье число - " + thirdNum(number) + ".");
+        }
+    }
